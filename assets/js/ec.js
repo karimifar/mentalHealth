9772a993b719e7af27c5e604b1bf843b921e8b23
Karimifar <em.karimifar@gmail.com>
2020-02-18 10:25:04 -0600

    fixed nav

---
+++ assets/js/ec.js
@@ -0,0 +1,11 @@
+$("#ec-table").append("LOADING DATA!")
+$.get("http://texashealthdata.com/api/tcmhcc/ec", function(data){
+        $("#ec-table").empty()
+        for (var i=0; i<data.length; i++){
+            var name= "<td class='name'>" + data[i][0] + "</td>";
+            var title= "<td class='title'>" + data[i][1] + "</td>";
+            var institution= "<td class='institution'>" + data[i][2]+ "</td>";
+            
+            $("#ec-table").append("<tr>"+name+title+institution+"</tr>")
+        }
+})
